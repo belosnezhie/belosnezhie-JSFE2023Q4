9769a9455c8ce965a72bf75c0faa512bb0e41ef7
Maria Treier <mariatraier@gmail.com>
2023-11-24 17:18:05 +0100

    feat: add next slide logic

---
+++ coffee-house/scripts/main-slider.js
@@ -0,0 +1,38 @@
+const sliderLine =  document.querySelector(".slider-line"),
+      prevButton = document.querySelector(".arrow-button-left"),
+      nextButton = document.querySelector(".arrow-button-right"),
+      progressBarItems = document.querySelectorAll(".progress-bar");
+
+let position = 0,
+    step = 480,
+    activeProgressIndex = 0;
+
+const findActiveProgressItem = (index) => {
+
+}
+
+const showNextSlide = () => {
+
+  if (position > -(progressBarItems.length - 1) * step) {
+    position -= step;
+  } else {
+    position = 0;
+  }
+
+  sliderLine.style.left = position + "px";
+}
+
+const showPrevSlide = () => {
+
+  if (position <= 0) {
+    position += step;
+  } else {
+    position = 0;
+  }
+
+  sliderLine.style.left = position + "px";
+}
+
+nextButton.addEventListener("click", showNextSlide);
+prevButton.addEventListener("click", showPrevSlide);
+
