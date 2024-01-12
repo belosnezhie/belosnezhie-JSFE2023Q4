d9182453165ab733eced510d2950cd060a415eb7
Maria Treier <mariatraier@gmail.com>
2024-01-12 19:08:33 +0100

    feat: implement modal window rendering logic

--- hangman/script.js
+++ hangman/script.js
@@ -52,8 +52,8 @@ function renderWord(parent, questionObj) {
   wordArr.forEach((item) => {
     const letter = document.createElement("span");
     letter.classList.add("letter");
-    // letter.classList.add("hidden-text");
-    // letter.innerText = item;
+    letter.classList.add("hidden-text");
+    letter.innerText = item;
     parent.append(letter);
   });
 }
@@ -81,6 +81,42 @@ function renderKeybord(parent) {
     parent.append(characterButton);
   });
 }
+
+function renderModal() {
+  const shadowWrapper = renderElement("div", "shadow-wrapper", document.body);
+  const modalWindow = renderElement("div", "modal-window", shadowWrapper);
+  const modalImg = renderElement("img", "lose-img", modalWindow);
+  modalImg.setAttribute("src", "assets/images/skull.png");
+  renderResults(modalWindow, "win");
+  renderResultWord(modalWindow);
+  const tryAgainButton = renderElement(
+    "button",
+    "try-again-button",
+    modalWindow,
+  );
+  tryAgainButton.innerText = "Try again!";
+}
+
+function renderResults(parent, result) {
+  const resultMessage = renderElement("p", "result-message", parent);
+  if (result === "win") {
+    resultMessage.innerText = "You win!";
+  } else {
+    resultMessage.innerText = "You lose!";
+  }
+}
+
+function renderResultWord(parent) {
+  const secretWord = renderElement("p", "secret-word", parent);
+  const letterArr = document.querySelectorAll(".letter");
+  let word = "";
+  for (let i = 0; i < letterArr.length; i++) {
+    word += letterArr[i].innerText;
+  }
+  secretWord.innerText = `Secret word: ${word.toUpperCase()}`;
+}
+
+renderModal();
 
 function randomQuestion(min, max) {
   let randonInteger = min + Math.random() * (max + 1 - min);
